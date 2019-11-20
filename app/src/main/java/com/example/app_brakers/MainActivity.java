51d38ddb3cfb779ba2fbb6ca2c624ea9febb1269
unknown <Admin@IHR80PBF251.continental.edu.pe>
2019-11-19 19:59:28 -0500

    Bekars

---
+++ app/src/main/java/com/example/app_brakers/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.app_brakers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnProducts = (Button) findViewById(R.id.btnEnter);
+        btnAboutsUs =(Button) findViewById(R.id.btnAboutUs);
+        btnProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.i(Settings.INFO,"Products");
+                Intent i = new Intent(MainActivity.this, ProductListActivity.class);
+                startActivity(i);
+            }
+        });
+        btnAboutsUs.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //Log.i(Settings.INFO,"About us");
+                Intent i = new Intent(MainActivity.this, AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(this,
+                "Aplicación Iniciada",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onStop(){
+        super.onStop();
+
+        Toast.makeText(this,
+                "Aplicacion Finalizada",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private Button btnProducts;
+    private Button btnAboutsUs;
+
+
+}
